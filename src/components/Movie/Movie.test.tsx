7562967d2fd6c665d9186cfa50bed6089b663507
Richard Quaite <richard@quaite.com>
2023-12-04 16:50:10 +0000

    Use a nested wrapper in unit test

--- src/components/Movie/Movie.test.tsx
+++ src/components/Movie/Movie.test.tsx
@@ -4,11 +4,20 @@ import { Table, TableBody } from '@mui/material';
 import GetMovies200 from '@/src/mocks/responses/GetMovies200';
 import GetMovieCompanies200 from '@/src/mocks/responses/GetMovieCompanies200';
 import { getAverage } from '@/src/lib/getAverage';
+import { PropsWithChildren } from 'react';
 
 const movie = GetMovies200[0];
 const movieCompany = GetMovieCompanies200.find(
   (movieCompany) => movieCompany.id === movie.filmCompanyId
 );
+
+const Wrapper = ({ children }: PropsWithChildren) => {
+  return (
+    <Table>
+      <TableBody>{children}</TableBody>
+    </Table>
+  );
+};
 
 describe('Movie', () => {
   test('It finds the mock movie', () => {
@@ -20,25 +29,15 @@ describe('Movie', () => {
   });
 
   test(`It displays a loading state`, async () => {
-    const { getAllByText } = render(
-      <Table>
-        <TableBody>
-          <Movie id={movie.id} />
-        </TableBody>
-      </Table>
-    );
+    const { getAllByText } = render(<Movie id={movie.id} />, {
+      wrapper: Wrapper,
+    });
 
     expect(getAllByText('...')).toHaveLength(3);
   });
 
   test(`It displays an error`, async () => {
-    const { getByText } = render(
-      <Table>
-        <TableBody>
-          <Movie id="-1" />
-        </TableBody>
-      </Table>
-    );
+    const { getByText } = render(<Movie id="-1" />, { wrapper: Wrapper });
 
     await waitFor(() =>
       expect(getByText('Movie not found')).toBeInTheDocument()
@@ -46,25 +45,13 @@ describe('Movie', () => {
   });
 
   test(`It displays the movie title (${movie!.title})`, async () => {
-    const { getByText } = render(
-      <Table>
-        <TableBody>
-          <Movie id={movie.id} />
-        </TableBody>
-      </Table>
-    );
+    const { getByText } = render(<Movie id={movie.id} />, { wrapper: Wrapper });
 
     await waitFor(() => expect(getByText(movie!.title)).toBeInTheDocument());
   });
 
   test('It displays the movie rating', async () => {
-    const { getByText } = render(
-      <Table>
-        <TableBody>
-          <Movie id={movie.id} />
-        </TableBody>
-      </Table>
-    );
+    const { getByText } = render(<Movie id={movie.id} />, { wrapper: Wrapper });
 
     await waitFor(() =>
       expect(getByText(getAverage(movie!.reviews))).toBeInTheDocument()
@@ -79,13 +66,9 @@ describe('Movie', () => {
     if (!movieWithNoReviews) {
       throw new Error('There are no movies without reviews in the mock api');
     }
-    const { getByText } = render(
-      <Table>
-        <TableBody>
-          <Movie id={movieWithNoReviews.id} />
-        </TableBody>
-      </Table>
-    );
+    const { getByText } = render(<Movie id={movieWithNoReviews.id} />, {
+      wrapper: Wrapper,
+    });
 
     await waitFor(() => expect(getByText('No reviews')).toBeInTheDocument());
   });
@@ -93,13 +76,7 @@ describe('Movie', () => {
   test(`It displays the movie company name(${
     movieCompany!.name
   })`, async () => {
-    const { getByText } = render(
-      <Table>
-        <TableBody>
-          <Movie id={movie.id} />
-        </TableBody>
-      </Table>
-    );
+    const { getByText } = render(<Movie id={movie.id} />, { wrapper: Wrapper });
 
     await waitFor(() =>
       expect(getByText(movieCompany!.name)).toBeInTheDocument()
